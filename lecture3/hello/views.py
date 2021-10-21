ca476deaa042c0e7d052e4200cb35f2155d6be78
DanielAdeyemi <adeyemidany@gmail.com>
2021-10-21 12:44:10 -0700

    update greet function to pass a name into html

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -13,4 +13,6 @@ def daniel(request):
 
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
