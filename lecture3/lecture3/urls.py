95edf1355ad32c1485c5e6397617f378060363e1
DanielAdeyemi <adeyemidany@gmail.com>
2021-10-21 12:12:58 -0700

    update project urls for hello app

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include path hello and include(link) all url patterns from that app
+    path('hello/', include("hello.urls"))
 ]
