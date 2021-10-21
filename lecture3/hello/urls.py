838ecb5995cd35baba9a46d4e9cff0b1c5668e26
DanielAdeyemi <adeyemidany@gmail.com>
2021-10-21 12:09:47 -0700

    create urls file for the hello app

---
+++ lecture3/hello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path with no additional argument and visit views.py -> function index, name for this path for easier reference later
+    path("", views.index, name="index")
+]
