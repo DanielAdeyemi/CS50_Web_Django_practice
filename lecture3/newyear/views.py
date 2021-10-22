53c253dac72a17f4f4b11f19a5a8c592c595f36b
DanielAdeyemi <adeyemidany@gmail.com>
2021-10-22 15:22:40 -0700

    fix typo in condition

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -7,5 +7,5 @@ import datetime
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.month == 1
+        "newyear": now.month == 1 and now.day == 1
     })
